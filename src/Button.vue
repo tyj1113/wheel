122be4b2dfd4f5b8b4d35722dd4b5269d5aeba29
tyj1113 <1991383016@qq.com>
2021-01-15 15:38:36 +0800

    Button组件改为单文件组件,使用parcel打包

---
+++ src/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="t-button"> hi </button>
+</template>
+
+<script >
+export default {
+name: "Button"
+}
+</script>
+
+<style lang="scss" scoped>
+.t-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+}
+.t-button:hover {
+  border-color: var(--border-color-hover);
+}
+.t-button:active {
+  background-color: var(--button-active-bg);
+}
+.t-button:focus {
+  outline: none;
+}
+</style>
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from "./Button.vue";
+
+Vue.component('t-button',Button)
+
+new Vue({
+    el:'#app',
+})
