255411bffb42f1db135d4c5512b9156579726123
tyj1113 <1991383016@qq.com>
2021-01-16 16:11:26 +0800

    入口文件与主页显示

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
-import Button from "./Button.vue";
+import Button from './Button.vue'
+import Icon from './Icon.vue'
+import Buttongroup from './Button-group.vue'
 
 Vue.component('t-button',Button)
+Vue.component('t-icon',Icon)
+Vue.component('t-button-group',Buttongroup)
 
 new Vue({
     el:'#app',
+    data:{
+        loading1:false,
+        loading2:false,
+        loading3:false,
+    }
 })
