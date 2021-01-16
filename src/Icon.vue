76afd8bfbc2062ab3802eae3ab9a641083628afa
tyj1113 <1991383016@qq.com>
2021-01-16 16:09:07 +0800

    封装icon组件

---
+++ src/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+      <svg class="t-icon">
+        <use :xlink:href="`#icon${name}`"></use>
+      </svg>
+</template>
+
+<script >
+export default {
+name: "Icon",
+  props:['name']
+}
+</script>
+
+<style lang="scss" scoped>
+.t-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
