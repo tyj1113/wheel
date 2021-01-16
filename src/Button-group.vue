d4e1a604187793ec715ab942326afe2b4eaf90a8
tyj1113 <1991383016@qq.com>
2021-01-16 16:10:25 +0800

    封装按扭组合组件

---
+++ src/Button-group.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="group">
+    <slot></slot>
+  </div>
+</template>
+
+<script >
+export default {
+  name: 'Button-group'
+};
+</script>
+
+<style lang="scss" >
+.group{
+  display: inline-flex;
+  .t-button{
+    border-radius: 0;
+    &:not(:first-child){
+      margin-left: -1px;
+    }
+    &:first-child{
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius:var(--border-radius);
+    }
+    &:last-child{
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius:var(--border-radius) ;
+    }
+    &:hover{
+      position: relative;
+      z-index:1;
+    }
+  }
+}
+</style>
