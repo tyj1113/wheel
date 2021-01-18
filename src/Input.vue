00f31d8f7e49ee42053a5ce129128b832368dcf8
tyj1113 <1991383016@qq.com>
2021-01-18 21:53:50 +0800

    添加Input组件完成样式,更新svg

---
+++ src/Input.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="wrapper" >
+      <input type="text" :value="value" :disabled="disabled" :readonly="readonly" :class="{error}">
+      <template v-if="error">
+        <icon name="jinggao" class="icon-error"></icon>
+        <span class="errorMessage">{{error}}</span>
+      </template>
+    </div>
+</template>
+
+<script >
+import Icon from "./Icon";
+export default {
+name: "Input",
+  components: {Icon},
+  props: {
+  value:{
+    type:String
+  },
+    disabled:{
+    type:Boolean,
+      default:false
+    },
+    readonly:{
+      type:Boolean,
+      default:false
+    },
+    error: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$red: #F1453D;
+.wrapper {
+  font-size: $font-size;
+  display: inline-flex;
+  align-items: center;
+  > :not(:last-child) {
+    margin-right: .5em;
+  }
+  > input {
+    height: 32px;
+    border: 1px solid $border-color;
+    border-radius: 4px; padding: 0 8px;
+    font-size: inherit;
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:focus {
+      box-shadow: inset 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
+    &[disabled], &[readonly] {
+      border-color: #bbb;
+      color: #bbb;
+      cursor: not-allowed;
+    }
+  }
+  >.error {
+      border-color: $red;
+  }
+  .icon-error {
+    fill: $red;
+  }
+  .errorMessage {
+    color: $red;
+  }
+}
+</style>
