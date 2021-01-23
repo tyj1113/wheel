8f52c49553f008bb7b906c61c3905a104e4cab6e
tyj1113 <1991383016@qq.com>
2021-01-23 16:57:03 +0800

    实现 toast 单例

--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,22 @@
 import Toast from './toast'
+let currentToast
+function createToast({Vue,message,propsData}){
+    let Constructor=Vue.extend(Toast)
+    let toast=new Constructor({
+        propsData
+    })
+    toast.$slots.default=[message]
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    return toast
+}
 export default {
     install(Vue,options){
         Vue.prototype.$toast=function (message,propOption){
-            let Constructor=Vue.extend(Toast)
-            let toast=new Constructor({
-                propsData:propOption
-            })
-            toast.$slots.default=[message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+            if(currentToast){
+                currentToast.close()
+            }
+            currentToast=createToast({Vue,message,propsData:propOption})
         }
     }
 }
