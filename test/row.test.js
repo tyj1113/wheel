bbdb89e5fff4bd5b9b092f046fbf1720dca4b3f5
tyj1113 <1991383016@qq.com>
2021-01-23 23:59:10 +0800

    修改之前的一个单元测试BUG  之前漏写一个括号竟然通过了 但是测试新的单元测试 就报错了

--- test/row.test.js
+++ test/row.test.js
@@ -6,15 +6,15 @@ import Col from '../src/Col'
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
-describe('Row',()=>{
+describe('Row', () => {
 
-    it('存在',()=>{
+    it('存在.', () => {
         expect(Row).to.exist
     })
-    it('接受gutter属性',()=>{
+    it('接收 gutter 属性.', (done) => {
         Vue.component('g-row', Row)
         Vue.component('g-col', Col)
-        const div=document.createElement('div')
+        const div = document.createElement('div')
         document.body.appendChild(div)
         div.innerHTML = `
       <g-row gutter="20">
@@ -22,33 +22,34 @@ describe('Row',()=>{
         <g-col span="12"></g-col>
       </g-row>
     `
-        const vm=new Vue({
-            el:div
+        const vm = new Vue({
+            el: div
         })
-setTimeout(()=>{
-    const row=vm.$el.querySelector('.row')
-    expect(getComputedStyle(row).marginLeft.to.eq('-10px'))
-    expect(getComputedStyle(row).marginRight.to.eq('-10px'))
-    const cols=vm.$el.querySelectorAll('.col')
-    expect(getComputedStyle(cols[0]).paddingRight).to.eq('10px')
-    expect(getComputedStyle(cols[1]).paddingLeft).to.eq('10px')
-    done()
-},0)
-        vm.$el.remove()
-        vm.$destroy()
+        setTimeout(() => {
+            const row = vm.$el.querySelector('.row')
+            expect(getComputedStyle(row).marginLeft).to.eq('-10px')
+            expect(getComputedStyle(row).marginRight).to.eq('-10px')
+            const cols = vm.$el.querySelectorAll('.col')
+            expect(getComputedStyle(cols[0]).paddingRight).to.eq('10px')
+            expect(getComputedStyle(cols[1]).paddingLeft).to.eq('10px')
+            done()
+            vm.$el.remove()
+            vm.$destroy()
+        })
     })
-    it('接收align属性',()=>{
-        const div=document.createElement('div')
+    it('接收 align 属性', () => {
+        const div = document.createElement('div')
         document.body.appendChild(div)
-        const constructor=Vue.extend(Row)
-        const vm=new constructor({
-            propsData:{
-                align:'center'
+        const Constructor = Vue.extend(Row)
+        const vm = new Constructor({
+            propsData: {
+                align: 'right'
             }
         }).$mount(div)
-        const row=vm.$el
-        expect(getComputedStyle(row).justifyContent).to.eq('center')
-        vm.$el.remove()
+        const element = vm.$el
+        expect(getComputedStyle(element).justifyContent).to.equal('flex-end')
+        div.remove()
         vm.$destroy()
     })
+
 })
