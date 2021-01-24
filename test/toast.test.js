9e192ec09c2af38b9fa812301e6514c8ec9e4058
tyj1113 <1991383016@qq.com>
2021-01-24 15:34:37 +0800

    解决测试用例报错 通过定时器 让单击关闭按钮排到nextTick 后面 避免nextTick获取不到按钮

--- test/toast.test.js
+++ test/toast.test.js
@@ -21,13 +21,12 @@ describe('Toast', () => {
                     autoClose: 1,
                 }
             }).$mount(div)
-
             vm.$on('close', () => {
                 expect(document.body.contains(vm.$el)).to.eq(false)
                 done()
             })
         })
-        it('接收 closeButton', () => {
+        it('接收 closeButton', (done) => {
             const callback = sinon.fake();
             const Constructor = Vue.extend(Toast)
             const vm = new Constructor({
@@ -38,11 +37,14 @@ describe('Toast', () => {
                     },
                 }
             }).$mount()
-            console.log(vm.$el.outerHTML)
             let closeButton = vm.$el.querySelector('.close')
             expect(closeButton.textContent.trim()).to.eq('关闭吧')
-            closeButton.click()
-            expect(callback).to.have.been.called
+            setTimeout(()=>{
+                closeButton.click()
+                expect(callback).to.have.been.called
+                done()
+            },0)
+
         })
         it('接收 enableHtml', () => {
             const Constructor = Vue.extend(Toast)
