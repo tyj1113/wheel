65688021c05f32d3d14e757b3f80eba7761e57a0
tyj1113 <1991383016@qq.com>
2021-01-31 16:55:55 +0800

    解决toast 自动关闭跟单击关闭重复触发

--- src/Toast.vue
+++ src/Toast.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="wrapper" :class="toastClass">
-    <div class="toast" ref="toast" >
+    <div class="toast" ref="toast">
       <div class="message">
         <slot v-if="!enableHtml"></slot>
         <div v-else v-html="$slots.default[0]"></div>
       </div>
       <div class="line" ref="line"></div>
-      <div  class="close" @click="clickClose">
+      <div class="close" @click="clickClose">
         {{ closeButton.text }}
       </div>
     </div>
@@ -17,11 +17,16 @@
 <script>
 export default {
   name: "Toast",
+  data() {
+    return {
+      timer: null
+    }
+  },
   props: {
     autoClose: {
       type: [Boolean, Number],
       default: 5,
-      validator (value) {
+      validator(value) {
         return value === false || typeof value === 'number';
       }
     },
@@ -57,7 +62,7 @@ export default {
   methods: {
     execAutoClose() {
       if (this.autoClose) {
-        setTimeout(() => {
+        this.timer = setTimeout(() => {
           this.close()
         }, this.autoClose * 1000)
       }
@@ -76,6 +81,7 @@ export default {
       console.log('调用toast方法')
     },
     clickClose() {
+      clearTimeout(this.timer)
       this.close()
       if (this.closeButton && typeof this.closeButton.callback === 'function') {
         this.closeButton.callback(this)
@@ -90,46 +96,72 @@ $font-size: 14px;
 $toast-min-height: 40px;
 $toast-bg: rgba(0, 0, 0, 0.75);
 @keyframes slide-up {
-  0% {opacity: 0; transform: translateY(100%);}
-  100% {opacity: 1;transform: translateY(0%);}
+  0% {
+    opacity: 0;
+    transform: translateY(100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0%);
+  }
 }
+
 @keyframes slide-down {
-  0% {opacity: 0; transform: translateY(-100%);}
-  100% {opacity: 1;transform: translateY(0%);}
+  0% {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0%);
+  }
 }
+
 @keyframes fade-in {
-  0% {opacity: 0; }
-  100% {opacity: 1;}
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
-.wrapper{
+
+.wrapper {
   position: fixed;
   left: 50%;
   transform: translateX(-50%);
   $animation-duration: 500ms;
+
   &.position-top {
     top: 0;
+
     .toast {
       border-top-left-radius: 0;
       border-top-right-radius: 0;
       animation: slide-down $animation-duration;
     }
   }
+
   &.position-bottom {
     bottom: 0;
+
     .toast {
       border-bottom-left-radius: 0;
       border-bottom-right-radius: 0;
       animation: slide-up $animation-duration;
     }
   }
+
   &.position-middle {
     top: 50%;
     transform: translateX(-50%) translateY(-50%);
+
     .toast {
       animation: fade-in $animation-duration;
     }
   }
 }
+
 .toast {
   font-size: $font-size;
   min-height: $toast-min-height;
@@ -142,9 +174,10 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
   padding: 0 16px;
   $animation-duration: 500ms;
+
   .message {
     padding: 8px 0;
-    word-break:break-word;
+    word-break: break-word;
   }
 
   .close {
