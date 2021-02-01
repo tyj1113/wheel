42fb2a709b34478d75245865c1d1f20669959f5b
tyj1113 <1991383016@qq.com>
2021-02-01 13:57:41 +0800

    添加展示代码

--- docs/.vuepress/components/Button-demos.vue
+++ docs/.vuepress/components/Button-demos.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <t-button>按钮</t-button>
+    <t-button>默认按钮</t-button>
+    <t-button icon="jinggao">默认按钮</t-button>
+    <t-button :loading="true">默认按钮</t-button>
+    <t-button disabled>默认按钮</t-button>
+
+    <pre>
+    <code>{{content}}</code>
+    </pre>
   </div>
 </template>
 
@@ -9,6 +16,16 @@ import Button from '../../../src/Button';
 export default {
   components:{
     't-button': Button
+  },
+  data () {
+    return {
+      content: `
+    <t-button>默认按钮</t-button>
+    <t-button icon="jinggao">默认按钮</t-button>
+    <t-button :loading="true">默认按钮</t-button>
+    <t-button disabled>默认按钮</t-button>
+      `
+    }
   }
 };
 </script>
