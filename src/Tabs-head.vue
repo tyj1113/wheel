cf74f2d6177304e4b2c6b9ea2784c62efadaec70
tyj1113 <1991383016@qq.com>
2021-02-04 02:15:09 +0800

    修复下划线位置错误

--- src/Tabs-head.vue
+++ src/Tabs-head.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-head">
+  <div class="tabs-head" ref="head">
     <slot></slot>
     <div class="line" ref="line"></div>
     <div class="actions">
@@ -19,8 +19,9 @@ export default {
     }
     this.eventHub.$on('update:selected', (name, vm) => {
       let {width, left} = vm.$el.getBoundingClientRect()
+      let {left:left2}=this.$refs.head.getBoundingClientRect()
       this.$refs.line.style.width = `${width}px`
-      this.$refs.line.style.transform = `translateX(${left}px)`
+      this.$refs.line.style.left = `${left-left2}px`
     })
   }
 }
