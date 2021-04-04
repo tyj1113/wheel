53da06a7acf44f9f3369ea6f5daf7b2d764c5154
tyj1113 <1991383016@qq.com>
2021-04-04 18:39:34 +0800

    改用vue-cli

--- src/Collapse-item.vue
+++ src/Collapse-item.vue
@@ -1,76 +1,47 @@
 <template>
-    <div class="collapseItem"  >
-      <div class="title" @click="onClick"  :data-name="name">{{title}}</div>
-      <div class="contentWrapper" v-if="show">
-        <slot></slot>
+  <div class="cascaderItem">
+    <div class="left">
+      <div class="label" v-for="item in items" @click="leftSelected = item">
+        {{item.name}}
       </div>
-
     </div>
+    <div class="right" v-if="rightItems">
+      <t-cascader-item :items="rightItems"></t-cascader-item>
+    </div>
+  </div>
 </template>
 
-<script >
+<script lang="ts">
 export default {
-name: "Collapse-item",
-  inject:['eventHub'],
-  data(){
-  return {
-    show:false,
-  }
-  },
-  mounted() {
-    this.eventHub.$on('update:selected',(names)=>{
-      this.show = names.indexOf(this.name) !== -1;
-    })
-  },
+  name: 'Cascader-item.vue',
   props:{
-  title:{
-    type:String
-  },
-    name:{
-      type:String
+    items:{
+      type:Array
     }
   },
-  methods:{
-    onClick(){
-      if(this.show){
-        this.eventHub.$emit('removeItem',this.name)
-      }else{
-        this.eventHub.$emit('addItem',this.name)
+  data () {
+    return {
+      leftSelected: null
+    }
+  },
+  computed: {
+    rightItems () {
+      if (this.leftSelected && this.leftSelected.children) {  //如果左边又被选中 并且有下一级 就返回下级对象
+        return this.leftSelected.children
+      } else {
+        return null
       }
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.collapseItem {
-  > .title {
-    border: 1px solid #bbb;
-    margin-top: -1px;
-    margin-left: -1px;
-    margin-right: -1px;
-    margin-bottom: -1px;
-    min-height: 32px;
-    display: flex;
-    align-items: center;
-    padding: 0 8px;
-  }
-
-  &:first-child {
-    > .title {
-      border-top-left-radius: 4px;
-      border-top-right-radius: 4px;
-    }
-  }
-
-  &:last-child {
-    > .title:last-child {
-      border-bottom-left-radius: 4px;
-      border-bottom-right-radius: 4px;
-    }
-  }
-  .contentWrapper{
-    padding: 8px;
+.cascaderItem {
+  display: flex;
+  align-items: flex-start;
+  .left {
+    border: 1px solid red;
   }
 }
 </style>
---
+++ src/demo.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+
+      <t-cascader :source="source" popover-height="200px"></t-cascader>
+
+    </div>
+</template>
+
+<script >
+import Cascader from './Cascader.vue';
+
+export default {
+  name: "demo",
+  data() {
+    return {
+      source: [
+        {
+          name: "浙江",
+          children: [
+            {
+              name: "杭州",
+              children: [{name: "上城"}, {name: "下城"}, {name: "江干"}]
+            },
+            {
+              name: "嘉兴",
+              children: [{name: "南湖"}, {name: "秀洲"}, {name: "嘉善"}]
+            }
+          ]
+        },
+        {
+          name: "福建",
+          children: [
+            {
+              name: "福州",
+              children: [{name: "鼓楼"}, {name: "台江"}, {name: "仓山"}]
+            }
+          ]
+        },
+        {
+          name: "安徽",
+          children: [
+            {
+              name: "合肥",
+              children: [
+                {
+                  name: "瑶海"
+                },
+                {
+                  name: "庐阳"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  },
+  components:{
+    't-cascader':Cascader
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+
+}
+
+#app {
+}
+</style>
