202123f41cd15c8b66f7f3b22d3757971262b00e
tyj1113 <1991383016@qq.com>
2021-06-05 16:42:04 +0800

    轮播初步实现

--- src/demo.vue
+++ src/demo.vue
@@ -1,115 +1,42 @@
 <template>
-    <div>
-
-      <t-cascader :source.sync="source" popover-height="200px" :selected.sync="selected"
-      :load-data="loadData" style="margin-bottom: 50px"
-      ></t-cascader>
-<!--      <br>-->
-<!--      <t-cascader :source.sync="source" popover-height="200px" :selected.sync="selected"-->
-<!--                  :load-data="loadData"-->
-<!--      ></t-cascader>-->
-
-    </div>
+  <div>
+    <TSlides class="wrapper" width="300px" height="200px" :selected.sync="selected">
+      <TSlidesItem name="1">
+        <div class="box">1</div>
+      </TSlidesItem>
+      <TSlidesItem name="2">
+        <div class="box">2</div>
+      </TSlidesItem>
+      <TSlidesItem name="3">
+        <div class="box">3</div>
+      </TSlidesItem>
+    </TSlides>
+  </div>
 </template>
-
-<script >
-import Cascader from './Cascader.vue';
-import db from './db'
-
-
-function ajax (parentId = 0) {
-  return new Promise((success, fail) => {
-    setTimeout(() => {
-      let result = db.filter((item) => item.parent_id === parentId)
-      result.forEach(node => {
-        if (db.filter(item => item.parent_id === node.id).length > 0) {
-          node.isLeaf = false
-        }else{
-          node.isLeaf = true
-        }
-      })
-      // console.log(result)
-      success(result)
-    }, 3000)//模拟异步获取数据
-  })
-}
+<script>
+import TSlides from './Slides'
+import TSlidesItem from './SlidesItem'
 export default {
   name: "demo",
-  created () {
-    ajax(0).then(result => {
-      this.source = result
-    })
-  },
-  data() {
+  components: {TSlides, TSlidesItem},
+  data () {
     return {
-      // source: [
-      //   {
-      //     name: "浙江",
-      //     children: [
-      //       {
-      //         name: "杭州",
-      //         children: [{name: "上城"}, {name: "下城"}, {name: "江干"}]
-      //       },
-      //       {
-      //         name: "嘉兴",
-      //         children: [{name: "南湖"}, {name: "秀洲"}, {name: "嘉善"}]
-      //       }
-      //     ]
-      //   },
-      //   {
-      //     name: "福建",
-      //     children: [
-      //       {
-      //         name: "福州",
-      //         children: [{name: "鼓楼"}, {name: "台江"}, {name: "仓山"}]
-      //       }
-      //     ]
-      //   },
-      //   {
-      //     name: "安徽",
-      //     children: [
-      //       {
-      //         name: "合肥",
-      //         children: [
-      //           {
-      //             name: "瑶海"
-      //           },
-      //           {
-      //             name: "庐阳"
-      //           }
-      //         ]
-      //       }
-      //     ]
-      //   }
-      // ],
-       source:[],
-      selected:[]
+      selected: '2'
     };
   },
-  methods: {
-    loadData ({id}, updateSource) {
-      ajax(id).then(result => {
-        updateSource(result) // 回调:把别人传给我的函数调用一下
-      })
-    },
-  },
-  components:{
-    't-cascader':Cascader
+  created () {
   }
-}
+};
 </script>
-
-<style lang="scss" scoped>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+<style>
+* {margin: 0; padding: 0; box-sizing: border-box;}
+.wrapper {
+  margin: 40px;
 }
-
-:root {
-
-}
-
-#app {
+.box {
+  width: 100%;
+  height: 350px;
+  background: #ddd;
+  border: 1px solid red;
 }
 </style>
