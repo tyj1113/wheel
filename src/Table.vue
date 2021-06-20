ff74b1917cd8da724cbed2a07cced40a9e8bb75a
tyj1113 <1991383016@qq.com>
2021-06-20 19:41:05 +0800

    实现固定表头，自定义列，可展开功能

--- src/Table.vue
+++ src/Table.vue
@@ -1,34 +1,58 @@
 <template>
-  <div class="t-table-wrapper">
-    <table class="t-table" :class="{bordered, compact, striped: striped}">
-      <thead>
-      <tr>
-        <th><input type="checkbox" @change="onChangeAllItems" ref="allChecked" :checked="areAllItemsSelected" /></th>
-        <th v-if="numberVisible">#</th>
-        <th v-for="column in columns" :key="column.field">
-          <div class="t-table-header">
-            {{column.text}}
-            <span v-if="column.field in orderBy" class="t-table-sorter" @click="changeOrderBy(column.field)">
+  <div class="t-table-wrapper" ref="wrapper">
+    <div :style="{height, overflowY: 'auto'}" ref="tableWrapper">
+      <table class="t-table" :class="{bordered, compact, striped: striped}" ref="table">
+        <thead>
+        <tr>
+          <th v-if="expendField" :style="{width: '50px'}" class="t-table-center"></th>
+          <th v-if="checkAble" :style="{width: '50px'}" class="t-table-center">
+            <input type="checkbox" @change="onChangeAllItems" ref="allChecked" :checked="areAllItemsSelected"/></th>
+          <th :style="{width: '50px'}" v-if="numberVisible">#</th>
+          <th :style="{width: column.width + 'px'}" v-for="column in columns" :key="column.field">
+            <div class="t-table-header">
+              {{ column.text }}
+              <span v-if="column.field in orderBy" class="t-table-sorter" @click="changeOrderBy(column.field)">
               <Icon name="up" :class="{active: orderBy[column.field] === 'asc'}"/>
               <Icon name="down" :class="{active: orderBy[column.field] === 'desc'}"/>
             </span>
-          </div>
-        </th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(item,index) in dataSource" :key="item.id">
-        <td>
-          <input type="checkbox" @change="onChangeItem(item, index, $event)"
-                 :checked="inSelectedItems(item)"
-          /></td>
-        <td v-if="numberVisible">{{index+1}}</td>
-        <template v-for="column in columns">
-          <td :key="column.field">{{item[column.field]}}</td>
+            </div>
+          </th>
+          <th ref="actionsHeader" v-if="$scopedSlots.default"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <template v-for="(item,index) in dataSource">
+          <tr :key="item.id">
+            <td v-if="expendField" :style="{width: '50px'}" class="t-table-center">
+
+              <Icon  v-if="inExpendedIds(item.id)" class="t-table-expendIcon" name="xia"
+                    @click="expendItem(item.id)"/>
+              <Icon  v-if="!inExpendedIds(item.id)" class="t-table-expendIcon" name="right"
+                     @click="expendItem(item.id)"/>
+            </td>
+            <td v-if="checkAble" :style="{width: '50px'}" class="t-table-center">
+              <input type="checkbox" @change="onChangeItem(item, index, $event)"
+                     :checked="inSelectedItems(item)"
+              /></td>
+            <td :style="{width: '50px'}" v-if="numberVisible">{{ index + 1 }}</td>
+            <template v-for="column in columns">
+              <td :style="{width: column.width + 'px'}" :key="column.field">{{ item[column.field] }}</td>
+            </template>
+            <td v-if="$scopedSlots.default">
+              <div ref="actions" style="display: inline-block;">
+                <slot :item="item"></slot>
+              </div>
+            </td>
+          </tr>
+          <tr v-if="inExpendedIds(item.id)" :key="`${item.id}-expend`">
+            <td :colspan="columns.length + expendedCellColSpan">
+              {{ item[expendField] || '空' }}
+            </td>
+          </tr>
         </template>
-      </tr>
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
     <div v-if="loading" class="t-table-loading">
       <Icon name="loading"/>
     </div>
@@ -41,7 +65,22 @@ import Icon from "@/Icon";
 export default {
   name: "Table",
   components: {Icon},
+  data() {
+    return {
+      expendedIds: []
+    }
+  },
   props: {
+    height: {
+      type: Number
+    },
+    expendField: {
+      type: String
+    },
+    checkAble: {
+      type: Boolean,
+      default: false
+    },
     orderBy: {
       type: Object,
       default: () => ({}),
@@ -69,7 +108,7 @@ export default {
     dataSource: {
       type: Array,
       required: true,
-      validator (array) {
+      validator(array) {
         return !(array.filter(item => item.id === undefined).length > 0)
       }
     },
@@ -82,28 +121,88 @@ export default {
       default: false
     }
   },
+  mounted() {
+    let table2 = this.$refs.table.cloneNode(false)
+    this.table2 = table2
+    table2.classList.add('t-table-copy')
+
+    let tHead = this.$refs.table.children[0]
+    let {height} = tHead.getBoundingClientRect()
+    this.$refs.tableWrapper.style.marginTop = height + 'px'
+    this.$refs.tableWrapper.style.height = this.height - height + 'px'
+    table2.appendChild(tHead)
+    this.$refs.wrapper.appendChild(table2)
+    if (this.$scopedSlots.default) {
+      let div = this.$refs.actions[0]//自定义列第一行单元格
+      let {width} = div.getBoundingClientRect()
+      let parent = div.parentNode
+      let styles = getComputedStyle(parent)
+      let paddingLeft = styles.getPropertyValue('padding-left')
+      let paddingRight = styles.getPropertyValue('padding-right')
+      let borderLeft = styles.getPropertyValue('border-left-width')
+      let borderRight = styles.getPropertyValue('border-right-width')
+      let {height} = this.$refs.table.getBoundingClientRect()
+
+      let scrollWidth = 0
+      if (height > this.height) {
+        let wrapper = this.$refs.tableWrapper
+        scrollWidth = wrapper.offsetWidth - wrapper.clientWidth
+      }
+      let width2 = width + parseInt(paddingRight) + parseInt(paddingRight) + parseInt(borderLeft) + parseInt(borderRight)
+      let theadWidth = width2 + scrollWidth
+      this.$refs.actionsHeader.style.width = theadWidth + 'px'
+      this.$refs.actions.map(div => {
+        div.parentNode.style.width = width2 + 'px'
+      })
+    }
+  },
+  beforeDestroy() {
+    this.table2.remove()
+  },
   watch: {
-    selectedItems () {
+    selectedItems() {
       if (this.selectedItems.length === this.dataSource.length) {
         this.$refs.allChecked.indeterminate = false
       } else this.$refs.allChecked.indeterminate = this.selectedItems.length !== 0;
     }
   },
   computed: {
-    areAllItemsSelected () {
+    areAllItemsSelected() {
       const a = this.dataSource.map(item => item.id).sort()
       const b = this.selectedItems.map(item => item.id).sort()
-      if (a.length !== b.length) { return false }
+      if (a.length !== b.length) {
+        return false
+      }
       let equal = true
       for (let i = 0; i < a.length; i++) if (a[i] !== b[i]) {
         equal = false
         break
       }
       return equal
+    },
+    expendedCellColSpan() {
+      let result = 0
+      if (this.checkAble) {
+        result += 1
+      }
+      if (this.expendField) {
+        result += 1
+      }
+      return result
     }
   },
   methods: {
-    changeOrderBy (key) {
+    inExpendedIds(id) {
+      return this.expendedIds.indexOf(id) >= 0
+    },
+    expendItem(id) {
+      if (this.inExpendedIds(id)) {
+        this.expendedIds.splice(this.expendedIds.indexOf(id), 1)
+      } else {
+        this.expendedIds.push(id)
+      }
+    },
+    changeOrderBy(key) {
       const copy = JSON.parse(JSON.stringify(this.orderBy))
       let oldValue = copy[key]
       if (oldValue === 'asc') {
@@ -115,10 +214,10 @@ export default {
       }
       this.$emit('update:orderBy', copy)
     },
-    inSelectedItems (item) {
+    inSelectedItems(item) {
       return this.selectedItems.filter(i => i.id === item.id).length > 0
     },
-    onChangeItem (item, index, e) {
+    onChangeItem(item, index, e) {
       let selected = e.target.checked
       let copy = JSON.parse(JSON.stringify(this.selectedItems))
       if (selected) {
@@ -128,7 +227,7 @@ export default {
       }
       this.$emit('update:selectedItems', copy)
     },
-    onChangeAllItems (e) {
+    onChangeAllItems(e) {
       let selected = e.target.checked
       this.$emit('update:selectedItems', selected ? this.dataSource : [])
     }
@@ -138,69 +237,84 @@ export default {
 
 <style scoped lang="scss">
 @import "../styles/_var";
+
 $grey: darken($grey, 10%);
 .t-table {
   width: 100%;
   border-collapse: collapse;
   border-spacing: 0;
   border-bottom: 1px solid $grey;
+
   &.bordered {
     border: 1px solid $grey;
+
     td, th {
       border: 1px solid $grey;
     }
   }
+
   &.compact {
     td, th {
       padding: 4px;
     }
   }
+
   td, th {
     border-bottom: 1px solid $grey;
     text-align: left;
     padding: 8px;
   }
+
   &.striped {
     tbody {
       > tr {
         &:nth-child(odd) {
           background: white;
         }
+
         &:nth-child(even) {
           background: lighten($grey, 10%);
         }
       }
     }
   }
+
   &-sorter {
     display: inline-flex;
     flex-direction: column;
     margin: 0 4px;
     cursor: pointer;
+
     svg {
       width: 10px;
       height: 10px;
       fill: $grey;
+
       &.active {
         fill: red;
       }
+
       &:first-child {
         position: relative;
         bottom: -1px;
       }
+
       &:nth-child(2) {
         position: relative;
         top: -1px;
       }
     }
   }
+
   &-header {
     display: flex;
     align-items: center;
   }
+
   &-wrapper {
     position: relative;
   }
+
   &-loading {
     background: rgba(255, 255, 255, 0.8);
     position: absolute;
@@ -211,6 +325,7 @@ $grey: darken($grey, 10%);
     display: flex;
     justify-content: center;
     align-items: center;
+
     svg {
       width: 50px;
       height: 50px;
@@ -218,5 +333,22 @@ $grey: darken($grey, 10%);
     }
   }
 
+  &-copy {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background: white;
+  }
+
+  &-expendIcon {
+    width: 10px;
+    height: 10px;
+  }
+
+  & &-center {
+    text-align: center;
+  }
 }
+
 </style>
