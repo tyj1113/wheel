9ffb18a41aab2dea3b425f2c33c603971f89f3a8
tyj1113 <1991383016@qq.com>
2021-07-04 13:06:53 +0800

    调整单元测试，但karma仍然无法运行

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -1,79 +1,73 @@
-import chai,{ expect } from 'chai'
+import chai, {expect} from 'chai'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
-
+import {shallowMount, mount} from '@vue/test-utils'
+import Button from '../../src/Button'
 chai.use(sinonChai)
-import Button from "@/Button";
-import Vue from "vue";
 
-describe('Button', () => {
+describe('Button.vue', () => {
     it('存在.', () => {
-        expect(Button).to.be.ok
+        expect(Button).to.exist
     })
     it('可以设置icon.', () => {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+        const wrapper = mount(Button, {
             propsData: {
                 icon: 'edit'
             }
-        }).$mount()
-        const useElement = vm.$el.querySelector('use')
-        expect(useElement.getAttribute('xlink:href')).to.equal('#iconedit')
-        vm.$destroy()
+        })
+        const useElement = wrapper.find('use')
+        expect(useElement.attributes()['href']).to.equal('#iconedit')
     })
     it('可以设置loading.', () => {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+        const wrapper = mount(Button, {
             propsData: {
                 icon: 'edit',
                 loading: true
             }
-        }).$mount()
+        })
+        const vm = wrapper.vm
         const useElements = vm.$el.querySelectorAll('use')
         expect(useElements.length).to.equal(1)
         expect(useElements[0].getAttribute('xlink:href')).to.equal('#iconloading')
-        vm.$destroy()
     })
-    // it('icon 默认的 order 是 0', () => {
-    //     const div = document.createElement('div')
-    //     document.body.appendChild(div)
-    //     const Constructor = Vue.extend(Button)
-    //     const vm = new Constructor({
-    //         propsData: {
-    //             icon: 'edit',
-    //         }
-    //     }).$mount(div)
-    //     const icon = vm.$el.querySelector('svg')
-    //     expect(getComputedStyle(icon).order).to.eq('0')
-    //     vm.$el.remove()
-    //     vm.$destroy()
-    // })
-    // it('设置 iconPosition 可以改变 order', () => {
-    //     const div = document.createElement('div')
-    //     document.body.appendChild(div)
-    //     const Constructor = Vue.extend(Button)
-    //     const vm = new Constructor({
-    //         propsData: {
-    //             icon: 'edit',
-    //             iconPosition: 'right'
-    //         }
-    //     }).$mount(div)
-    //     const icon = vm.$el.querySelector('svg')
-    //     expect(getComputedStyle(icon).order).to.eq('2')
-    //     vm.$el.remove()
-    //     vm.$destroy()
-    // })
-    it('点击 button 触发 click 事件', () => {
-        const Constructor = Vue.extend(Button)
-        const vm = new Constructor({
+
+    it('icon 默认的 order 是 1', () => {
+        const wrapper = mount(Button, {
+            attachToDocument: true,
             propsData: {
                 icon: 'edit',
             }
-        }).$mount()
+        })
+        const vm = wrapper.vm
+        const icon = vm.$el.querySelector('svg')
+        expect(getComputedStyle(icon).order).to.eq('1')
+    })
+
+    it('设置 iconPosition 可以改变 order', () => {
+        const wrapper = mount(Button, {
+            attachToDocument: true,
+            propsData: {
+                icon: 'edit',
+                iconPosition: 'right'
+            }
+        })
+        const vm = wrapper.vm
+        const icon = vm.$el.querySelector('svg')
+        expect(getComputedStyle(icon).order).to.eq('2')
+    })
+    it('点击 button 触发 click 事件', () => {
+        const wrapper = mount(Button, {
+            propsData: {
+                icon: 'edit',
+            }
+        })
+        const vm = wrapper.vm
+
 
         const callback = sinon.fake();
         vm.$on('click', callback)
         vm.$el.click()
         expect(callback).to.have.been.called
+
     })
 })
